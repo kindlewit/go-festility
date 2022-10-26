daf4b3a187c6b37b92adc5218c7e0290fbc3e6fc
datmemerboi <shravanssd@gmail.com>
2022-10-26 17:07:46 +1100

    refactor: Tabs into spaces

--- constants.go
+++ constants.go
@@ -1,15 +1,15 @@
 package main
 
 const (
-	DuplicateRecord			=	"duplicate"
-	ScheduleIDLen				= 3
-	DefaultScheduleID		=	"1001"
-	// Time formats
-	DateInputParse			=	"2006-01-02"
-	DateAsWordsParse		=	"January 02, 2006"
-	TimeZoneName				=	"Asia/Kolkata"
-	// Slot types:
-	TypeMovie						=	"movie"
-	TypeInterview				= "interview"
-	TypeOther						=	"other"
+  DuplicateRecord   = "duplicate"
+  ScheduleIDLen     = 3
+  DefaultScheduleID = "1001"
+  // Time formats
+  DateInputParse    = "2006-01-02"
+  DateAsWordsParse  = "January 02, 2006"
+  TimeZoneName      = "Asia/Kolkata"
+  // Slot types:
+  TypeMovie         = "movie"
+  TypeInterview     = "interview"
+  TypeOther         = "other"
 )
