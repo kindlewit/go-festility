fdae072991b971703468e066466279005f60c9d1
datmemerboi <shravanssd@gmail.com>
2022-12-02 20:30:41 +1100

    fix: Tab spacing

--- models/movie.go
+++ models/movie.go
@@ -27,11 +27,11 @@ type Movie struct {
 }
 
 type MovieSlot struct {
-	Directors       []string    `bson:"directors" json:"directors,omitempty"`
+  Directors       []string    `bson:"directors" json:"directors,omitempty"`
   OriginalTitle   string      `bson:"original_title" json:"original_title,omitempty"`
   Genres          []string    `bson:"genres" json:"genres,omitempty"`
   Languages       []Language  `bson:"languages" json:"languages,omitempty"`
   Countries       []Country   `bson:"countries" json:"countries,omitempty"`
   MovieId         int         `bson:"movie_id" json:"movie_id"`
-	Slot
+  Slot
 }
