1e69d1b37aca004b30fc57a8bd11245fa6ca1834
datmemerboi <shravanssd@gmail.com>
2022-12-02 20:25:29 +1100

    add: Models package containing all schema

---
+++ models/festival.go
@@ -0,0 +1,38 @@
+package models
+
+type Fest struct {
+  Id      string    `bson:"id" form:"id" json:"id" binding:"required"`
+  Name    string    `bson:"name" form:"name" json:"name" binding:"required"`
+  From    int       `bson:"from_date" form:"from_date" json:"from_date" binding:"required"`
+  To      int       `bson:"to_date" form:"to_date" json:"to_date" binding:"required"`
+  Url     string    `bson:"url" form:"url" json:"url"`
+}
+
+type Slot struct {
+  // Common for both
+  Id              string      `bson:"id" json:"id"`
+  Type            string      `bson:"slot_type" binding:"required" json:"slot_type" binding:"required"`
+  ScheduleID      string      `bson:"schedule_id" json:"schedule_id"`
+  Title           string      `bson:"title" json:"title"`
+  Synopsis        string      `bson:"synopsis" json:"synopsis"`
+  Start           int         `bson:"start_time" json:"start_time"`
+  Duration        int         `bson:"duration" json:"duration"`
+  // Movie specific
+  Directors       []string    `bson:"directors" json:"directors,omitempty"`
+  OriginalTitle   string      `bson:"original_title" json:"original_title,omitempty"`
+  Genres          []string    `bson:"genres" json:"genres,omitempty"`
+  Languages       []Language  `bson:"languages" json:"languages,omitempty"`
+  Countries       []Country   `bson:"countries" json:"countries,omitempty"`
+  MovieId         int         `bson:"movie_id" json:"movie_id"`
+}
+
+type Schedule struct {
+  Id          string  `bson:"id" json:"id"`
+  Fest        string  `bson:"fest_id" json:"fest_id"`
+  Custom      bool    `bson:"custom" json:"custom"`
+}
+
+type CustomSchedule struct {
+  Username    string  `bson:"username" json:"username"`
+  Schedule
+}
