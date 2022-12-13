4484fa0876a605073308d06043d96db1a0477f9e
datmemerboi <shravanssd@gmail.com>
2022-12-13 18:48:17 +1100

    feat: GetScreensInBulk - screens for fest

--- services/screen-service.go
+++ services/screen-service.go
@@ -51,6 +51,41 @@ func GetScreen(client *mongo.Client, screenID string) (data models.Screen, err e
   return data, nil;
 }
 
+// Fetches multiple screen records.
+func GetScreensInBulk(client *mongo.Client, screenIDlist []string) (data []models.Screen, err error) {
+  collection := client.Database("festility").Collection("screen"); // Collection to use
+  query := bson.M{ "id": bson.M{ "$in": screenIDlist } };
+
+  // Create context
+  ctx, cancel := context.WithTimeout(context.Background(), constants.QueryTimeout);
+  defer cancel();
+
+  cursor, err := collection.Find(ctx, query);
+  if err != nil {
+    fmt.Println(err.Error());
+    return data, constants.DetermineError(err);
+  }
+  defer cursor.Close(ctx);
+
+  for cursor.Next(ctx) { // Iterate cursor
+    var d models.Screen;
+
+    err = cursor.Decode(&d); // Decode cursor data into model
+    if err != nil {
+      fmt.Println(err.Error());
+      return data, constants.ErrDataParse;
+    }
+
+    data = append(data, d); // Push data record into array
+  }
+  if err = cursor.Err(); err != nil {
+    fmt.Println(err.Error());
+    return data, constants.DetermineError(err);
+  }
+
+  return data, nil;
+}
+
 // Fetches screens for a cinema by cinema id.
 func GetCinemaScreens(client *mongo.Client, cinemaID string) (data []models.Screen, err error) {
   collection := client.Database("festility").Collection("screen"); // Collection to use
