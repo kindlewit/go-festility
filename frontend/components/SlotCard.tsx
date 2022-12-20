1bd3087c56ca2479c178a41923e1b3dba622291f
datmemerboi <shravanssd@gmail.com>
2022-12-20 18:09:10 +1100

    feat: Add dayjs parsing for SlotCard

--- frontend/components/SlotCard.tsx
+++ frontend/components/SlotCard.tsx
@@ -1,31 +1,48 @@
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, startTransition } from "react";
 import { SlotStruct } from "../entities/SlotType";
 import styles from "../styles/Home.module.css";
+import { DEF_TIMEZONE } from "../constants";
+import dayjs, { Dayjs } from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
 
-export function timeToValue(time: Date): number {
-  return time.getHours() + time.getMinutes() / 60;
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export function timeToValue(time: Dayjs): number {
+  return time.hour() + time.minute() / 60;
 }
 
 const SlotCard: FC<SlotStruct> = (slot): ReactElement => {
-  let startTime = new Date(slot.start_time);
-  const startTimeAsValue = timeToValue(startTime);
-  let directors = slot?.directors?.join(' \u2022 ');
+  let slotStartInIST: Dayjs = dayjs.unix(slot.start_time).tz(DEF_TIMEZONE);
+  let slotEndInIST: Dayjs = dayjs
+    .unix(slot.start_time + slot.duration * 60)
+    .tz(DEF_TIMEZONE);
+
+  let slotPosForCss = {
+    x: slotStartInIST.hour() + slotStartInIST.minute() / 60,
+  };
+
+  let concatDirectors = slot?.directors?.join(" \u2022 ");
+
   return (
     <div
       className={styles.slot}
       style={{
         position: "relative",
-        top: "-300px",
-        left: `calc(${startTimeAsValue} * 124px)`,
+        top: "-500px",
+        left: `calc(${slotPosForCss.x} * 124px)`,
         width: `calc(${slot.duration / 60} * 124px)`,
       }}
     >
       <div className={styles.slot_details}>
-        <span>{startTime.toDateString()}</span>
+        <span>
+          {slotStartInIST.format("hh:mm A")} – {slotEndInIST.format("hh:mm A")}
+        </span>
         <span>
           <strong>{slot.title}</strong>
         </span>
-        <span>{directors}</span>
+        <span className={styles.slot__director}>{concatDirectors}</span>
         <span className={styles.slot__duration}>{slot.duration}'</span>
       </div>
     </div>
