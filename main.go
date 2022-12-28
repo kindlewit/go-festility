b7b2cc230a7b416c16d1f0932192fa1a5c2f3061
datmemerboi <shravanssd@gmail.com>
2022-12-28 18:58:08 +1100

    fix: Extract routing for api testing purpose

--- main.go
+++ main.go
@@ -2,32 +2,14 @@ package main
 
 import (
   "github.com/gin-gonic/gin"
-  "festility/handlers"
-  // "festility/services"
+  "github.com/kindlewit/go-festility/router"
 )
 
 func main() {
-  router := gin.Default() // Equivalent to app in express
-
-  // Bind routes & handlers
-  router.GET("/", handlers.IndexHandler);
-
-  router.GET("/movie/:id", handlers.GetMovieHandler);
-
-  router.POST("/fest", handlers.CreateFestivalHandler);
-  router.GET("/fest/:id", handlers.GetFestHandler);
-
-  router.GET("/fest/:id/screen", handlers.GetFestScreensHandler);
-
-  router.POST("/fest/:id/schedule", handlers.CreateScheduleHandler);
-  router.GET("/fest/:id/schedule", handlers.GetDailyScheduleHandler);
-  router.GET("/fest/:id/schedule/:sid", handlers.GetScheduleHandler);
-
-  router.POST("/cinema", handlers.CreateCinemaHandler);
-  router.GET("/cinema/:id", handlers.GetCinemaHandler);
-  router.POST("/cinema/:id/screen", handlers.AddCinemaScreensHandler);
-  router.GET("/cinema/:id/screen", handlers.GetCinemaScreensHandler);
+  app := gin.Default() // Equivalent to app in express
 
   // services.Migrate(); // Migrate to setup db schema
-  router.Run(); // Start the server
+  router.SetupRouter(app);
+
+  app.Run() // Start the server
 }
