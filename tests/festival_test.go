42488baca13dd05ce97650302f3bcc0d80c3f391
datmemerboi <shravanssd@gmail.com>
2022-12-29 18:56:03 +1100

    test: Add festival tests

---
+++ tests/festival_test.go
@@ -0,0 +1,98 @@
+package tests
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kindlewit/go-festility/constants"
+	"github.com/kindlewit/go-festility/models"
+	"github.com/kindlewit/go-festility/router"
+	"github.com/stretchr/testify/assert"
+)
+
+var FEST_DATA_CORRECT = models.Fest{
+	Id:   "Fest101",
+	Name: "Test Fest 101",
+	From: 1672294677,
+	To:   1672467473,
+}
+
+func Test_WhenEmptyCreateFest_ShouldRetError(t *testing.T) {
+	app := gin.Default()
+	router.SetupRouter(app)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/fest", nil)
+	app.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code, "Received different response status:")
+	assert.Equal(t, w.Body.String(), constants.MsgMissingFestParams)
+}
+
+func Test_WhenCreateFest_ShouldRetSuccess(t *testing.T) {
+	app := gin.Default()
+	router.SetupRouter(app)
+
+	w := httptest.NewRecorder()
+
+	var body bytes.Buffer
+	err := json.NewEncoder(&body).Encode(FEST_DATA_CORRECT)
+	if err != nil {
+		t.FailNow()
+	}
+
+	req, _ := http.NewRequest(http.MethodPost, "/fest", &body)
+	app.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusCreated, w.Code, "Received different response status:")
+	assert.Contains(t, w.Body.String(), FEST_DATA_CORRECT.Id)
+}
+
+func Test_WhenDuplicateCreateFest_ShouldRetError(t *testing.T) {
+	app := gin.Default()
+	router.SetupRouter(app)
+
+	w := httptest.NewRecorder()
+
+	var body bytes.Buffer
+	err := json.NewEncoder(&body).Encode(FEST_DATA_CORRECT)
+	if err != nil {
+		t.FailNow()
+	}
+
+	req, _ := http.NewRequest(http.MethodPost, "/fest", &body)
+	app.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusConflict, w.Code, "Received different response status:")
+	assert.Equal(t, w.Body.String(), constants.MsgDuplicateRecord)
+}
+
+func Test_WhenGetFest_ShouldRetData(t *testing.T) {
+	app := gin.Default()
+	router.SetupRouter(app)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/fest/%s", FEST_DATA_CORRECT.Id), nil)
+	app.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code, "Received different response status!")
+	assert.Contains(t, w.Body.String(), FEST_DATA_CORRECT.Id)
+	assert.Contains(t, w.Body.String(), FEST_DATA_CORRECT.Name)
+}
+
+func Test_WhenGetInvalidFest_ShouldRetError(t *testing.T) {
+	app := gin.Default()
+	router.SetupRouter(app)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/fest/Nope", nil)
+	app.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusNotFound, w.Code, "Received different response status!")
+	assert.Equal(t, constants.MsgNoSuchRecord, w.Body.String())
+}
