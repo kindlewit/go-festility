b28c55aa4bd74c3251e9c5274a999e91dfceef9e
datmemerboi <shravanssd@gmail.com>
2022-12-29 18:53:14 +1100

    fix: Remove return

--- router/router.go
+++ router/router.go
@@ -1,29 +1,27 @@
 package router
 
 import (
-  "github.com/gin-gonic/gin"
-  "github.com/kindlewit/go-festility/handlers"
+	"github.com/gin-gonic/gin"
+	"github.com/kindlewit/go-festility/handlers"
 )
 
-func SetupRouter(router *gin.Engine) *gin.Engine {
-  // Bind routes & handlers
-  router.GET("/", handlers.IndexHandler)
+func SetupRouter(router *gin.Engine) {
+	// Bind routes & handlers
+	router.GET("/", handlers.IndexHandler)
 
-  router.GET("/movie/:id", handlers.GetMovieHandler)
+	router.GET("/movie/:id", handlers.GetMovieHandler)
 
-  router.POST("/fest", handlers.CreateFestivalHandler)
-  router.GET("/fest/:id", handlers.GetFestHandler)
+	router.POST("/fest", handlers.CreateFestivalHandler)
+	router.GET("/fest/:id", handlers.GetFestHandler)
 
-  router.GET("/fest/:id/screen", handlers.GetFestScreensHandler)
+	router.GET("/fest/:id/screen", handlers.GetFestScreensHandler)
 
-  router.POST("/fest/:id/schedule", handlers.CreateScheduleHandler)
-  router.GET("/fest/:id/schedule", handlers.GetDailyScheduleHandler)
-  router.GET("/fest/:id/schedule/:sid", handlers.GetScheduleHandler)
+	router.POST("/fest/:id/schedule", handlers.CreateScheduleHandler)
+	router.GET("/fest/:id/schedule", handlers.GetDailyScheduleHandler)
+	router.GET("/fest/:id/schedule/:sid", handlers.GetScheduleHandler)
 
-  router.POST("/cinema", handlers.CreateCinemaHandler)
-  router.GET("/cinema/:id", handlers.GetCinemaHandler)
-  router.POST("/cinema/:id/screen", handlers.AddCinemaScreensHandler)
-  router.GET("/cinema/:id/screen", handlers.GetCinemaScreensHandler)
-
-  return router
+	router.POST("/cinema", handlers.CreateCinemaHandler)
+	router.GET("/cinema/:id", handlers.GetCinemaHandler)
+	router.POST("/cinema/:id/screen", handlers.AddCinemaScreensHandler)
+	router.GET("/cinema/:id/screen", handlers.GetCinemaScreensHandler)
 }
