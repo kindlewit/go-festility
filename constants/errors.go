5f628fc766a7f1286086a560fbc7eac2176552c4
datmemerboi <shravanssd@gmail.com>
2022-12-29 18:52:12 +1100

    style: Response message constants

--- constants/errors.go
+++ constants/errors.go
@@ -1,65 +1,85 @@
 package constants
 
 import (
-  "errors"
-  "strings"
-  "net/http"
+	"errors"
+	"net/http"
+	"strings"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 var (
-  ErrDuplicateRecord error = errors.New("ErrDuplicateRecord");
-  ErrNoSuchRecord error = errors.New("ErrNoSuchRecord");
-  ErrConnection error = errors.New("ErrConnection");
-  ErrMongo error = errors.New("ErrMongo");
-  ErrDataParse error = errors.New("ErrDataParse");
-  ErrEmptyData error = errors.New("ErrEmptyData");
-  ErrNoDefaultSchedule error = errors.New("ErrNoDefaultSchedule");
-  ErrApiFetch error = errors.New("ErrApiFetch");
-  ErrApiParse error = errors.New("ErrApiParse");
+	ErrDuplicateRecord   error = errors.New("ErrDuplicateRecord")
+	ErrNoSuchRecord      error = errors.New("ErrNoSuchRecord")
+	ErrConnection        error = errors.New("ErrConnection")
+	ErrMongo             error = errors.New("ErrMongo")
+	ErrDataParse         error = errors.New("ErrDataParse")
+	ErrEmptyData         error = errors.New("ErrEmptyData")
+	ErrNoDefaultSchedule error = errors.New("ErrNoDefaultSchedule")
+	ErrApiFetch          error = errors.New("ErrApiFetch")
+	ErrApiParse          error = errors.New("ErrApiParse")
+)
+
+var (
+	MsgMissingFestParams string = "Required parameters missing: id/name/from_date/to_date."
+	MsgNoSuchRecord      string = "No such record exists. Please check the record id."
+	MsgInconsistentId    string = "Record was created but found an inconsistency in record id."
+	MsgDuplicateRecord   string = "Request to create duplicate record. Please check the record id."
+	MsgConnectionFailure string = "Failed to connect to database. Please try again later."
+	MsgDatabaseFailure   string = "Faced a database error. Please try again later."
+	MsgDataParse         string = "Faced an error while parsing internal data. Please try again."
+	MsgEmptyData         string = "Data being requested does not exist."
+	MsgNoSchedule        string = "No schedule available for this fest yet."
 )
 
 // Determines which custom error to throw based on error received.
-func DetermineError(err error) (error) {
-  if (strings.Contains(err.Error(), "server selection error: context deadline exceeded")) {
-    // Caught DB connection error
-    return ErrConnection;
-  }
-  if (err.Error() == "mongo: no documents in result") {
-    // Caught a "mongo: no documents in result" error
-    return ErrNoSuchRecord;
-  }
+func DetermineError(err error) error {
+	if strings.Contains(err.Error(), "server selection error: context deadline exceeded") {
+		// Caught DB connection error
+		return ErrConnection
+	}
+	if err.Error() == "mongo: no documents in result" {
+		// Caught a "mongo: no documents in result" error
+		return ErrNoSuchRecord
+	}
 
-  return ErrMongo;
+	return ErrMongo
 }
 
 // Handles error by returning appropriate HTTP response code.
 func HandleError(c *gin.Context, err error) {
-  switch err {
-  case ErrNoSuchRecord: {
-    c.JSON(http.StatusNotFound, "No such record exists. Please check the record id.");
-  }
-  case ErrDuplicateRecord: {
-    c.JSON(http.StatusConflict, "Request to create duplicate record. Please check the record id.");
-  }
-  case ErrConnection: {
-    c.JSON(http.StatusInternalServerError, "Failed to connect to database. Please try again later.");
-  }
-  case ErrMongo: {
-    c.JSON(http.StatusInternalServerError, "Faced a database error. Please try again later.");
-  }
-  case ErrDataParse: {
-    c.JSON(http.StatusInternalServerError, "Faced an error while parsing internal data. Please try again.");
-  }
-  case ErrEmptyData: {
-    c.JSON(http.StatusNotFound, "Data being requested does not exist.");
-  }
-  case ErrNoDefaultSchedule: {
-    c.JSON(http.StatusNotFound, "No schedule available for this fest yet.")
-  }
-  default: {
-    c.JSON(http.StatusInternalServerError, "Unable to process request. Please try again.");
-  }
-  }
+	switch err {
+	case ErrNoSuchRecord:
+		{
+			c.String(http.StatusNotFound, MsgNoSuchRecord)
+		}
+	case ErrDuplicateRecord:
+		{
+			c.String(http.StatusConflict, MsgDuplicateRecord)
+		}
+	case ErrConnection:
+		{
+			c.String(http.StatusInternalServerError, MsgConnectionFailure)
+		}
+	case ErrMongo:
+		{
+			c.String(http.StatusInternalServerError, MsgDatabaseFailure)
+		}
+	case ErrDataParse:
+		{
+			c.String(http.StatusInternalServerError, MsgDataParse)
+		}
+	case ErrEmptyData:
+		{
+			c.String(http.StatusNotFound, MsgEmptyData)
+		}
+	case ErrNoDefaultSchedule:
+		{
+			c.String(http.StatusNotFound, MsgNoSchedule)
+		}
+	default:
+		{
+			c.String(http.StatusInternalServerError, "Unable to process request. Please try again.")
+		}
+	}
 }
--- handlers/festival-handler.go
+++ handlers/festival-handler.go
@@ -1,96 +1,96 @@
 package handlers
 
 import (
-  "net/http"
+	"net/http"
 
-  "github.com/gin-gonic/gin"
-  "github.com/kindlewit/go-festility/models"
-  "github.com/kindlewit/go-festility/services"
-  "github.com/kindlewit/go-festility/constants"
-  "github.com/kindlewit/go-festility/utils"
+	"github.com/gin-gonic/gin"
+	"github.com/kindlewit/go-festility/constants"
+	"github.com/kindlewit/go-festility/models"
+	"github.com/kindlewit/go-festility/services"
+	"github.com/kindlewit/go-festility/utils"
 )
 
 // Handles request to create one festival.
 func CreateFestivalHandler(c *gin.Context) {
-  var body models.Fest;
-  var success bool;
-  var err error;
+	var body models.Fest
+	var success bool
+	var err error
 
-  if err = c.BindJSON(&body); err != nil {
-    c.String(http.StatusBadRequest, "Required parameters missing: id/name/from_date/to_date.");
-    return;
-  }
+	if err = c.BindJSON(&body); err != nil {
+		c.String(http.StatusBadRequest, constants.MsgMissingFestParams)
+		return
+	}
 
-  client := services.Connect();
-  success, err = services.CreateFestival(client, body);
-  defer services.Disconnect(client);
+	client := services.Connect()
+	success, err = services.CreateFestival(client, body)
+	defer services.Disconnect(client)
 
-  if err != nil {
-    constants.HandleError(c, err);
-    return;
-  }
-  if (!success) {
-    c.String(http.StatusConflict, "Record was created but found an inconsistency in record id.");
-    return;
-  }
+	if err != nil {
+		constants.HandleError(c, err)
+		return
+	}
+	if !success {
+		c.String(http.StatusConflict, MsgInconsistentId)
+		return
+	}
 
-  c.JSON(http.StatusCreated, gin.H{ "id": body.Id });
+	c.JSON(http.StatusCreated, gin.H{"id": body.Id})
 }
 
 // Handles request to get details of one festival.
 func GetFestHandler(c *gin.Context) {
-  festId := c.Param("id");
+	festId := c.Param("id")
 
-  client := services.Connect();
-  resp, err := services.GetFestival(client, festId);
-  defer services.Disconnect(client);
+	client := services.Connect()
+	resp, err := services.GetFestival(client, festId)
+	defer services.Disconnect(client)
 
-  if err != nil {
-    constants.HandleError(c, err);
-    return;
-  }
+	if err != nil {
+		constants.HandleError(c, err)
+		return
+	}
 
-  c.JSON(http.StatusOK, resp);
-  return;
+	c.JSON(http.StatusOK, resp)
+	return
 }
 
 // Handles request to get all screens of one festival.
 func GetFestScreensHandler(c *gin.Context) {
-  var err error;
-  var resp []models.CinemaScreen;
+	var err error
+	var resp []models.CinemaScreen
 
-  festID := c.Param("id");
+	festID := c.Param("id")
 
-  client := services.Connect();
-  scheduleID, err := services.GetDefaultScheduleID(client, festID);
-  if err != nil {
-    constants.HandleError(c, err);
-    return;
-  }
-  screenIDList, err := services.GetSlotScreensOfSchedule(client, scheduleID);
-  if err != nil {
-    constants.HandleError(c, err);
-    return;
-  }
-  screenList, err := services.GetScreensInBulk(client, screenIDList);
-  if err != nil {
-    constants.HandleError(c, err);
-    return;
-  }
+	client := services.Connect()
+	scheduleID, err := services.GetDefaultScheduleID(client, festID)
+	if err != nil {
+		constants.HandleError(c, err)
+		return
+	}
+	screenIDList, err := services.GetSlotScreensOfSchedule(client, scheduleID)
+	if err != nil {
+		constants.HandleError(c, err)
+		return
+	}
+	screenList, err := services.GetScreensInBulk(client, screenIDList)
+	if err != nil {
+		constants.HandleError(c, err)
+		return
+	}
 
-  cinemaHashMap := make(map[string]models.Cinema);
+	cinemaHashMap := make(map[string]models.Cinema)
 
-  for i := 0; i < len(screenList); i++ {
-    cID := screenList[i].CinemaID;
-    if cinemaData, isPresent := cinemaHashMap[cID]; isPresent {
-      resp = append(resp, utils.BindCinemaToScreen(screenList[i], cinemaData));
-    } else {
-      cinemaHashMap[cID], err = services.GetCinema(client, cID);
-      resp = append(resp, utils.BindCinemaToScreen(screenList[i], cinemaHashMap[cID]));
-    }
-  }
-  // cinemaList, err := services.GetCinemasInBulk(client, cinemaHashMap.Keys());
-  defer services.Disconnect(client);
+	for i := 0; i < len(screenList); i++ {
+		cID := screenList[i].CinemaID
+		if cinemaData, isPresent := cinemaHashMap[cID]; isPresent {
+			resp = append(resp, utils.BindCinemaToScreen(screenList[i], cinemaData))
+		} else {
+			cinemaHashMap[cID], err = services.GetCinema(client, cID)
+			resp = append(resp, utils.BindCinemaToScreen(screenList[i], cinemaHashMap[cID]))
+		}
+	}
+	// cinemaList, err := services.GetCinemasInBulk(client, cinemaHashMap.Keys());
+	defer services.Disconnect(client)
 
-  c.JSON(http.StatusOK, resp);
+	c.JSON(http.StatusOK, resp)
 }
