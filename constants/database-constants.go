451b90c5cb6711fdd3b3e400df3edbed9af71315
datmemerboi <shravanssd@gmail.com>
2024-01-11 21:52:55 +1100

    feat: constant DB table names

---
+++ constants/database-constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	DatabaseName  = "festility"
+	TableFestival = "festival"
+	TableCinema   = "cinema"
+	TableSchedule = "schedule"
+	TableSlot     = "slot"
+	TableScreen   = "screen"
+)
